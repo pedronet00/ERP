bf4d2958edcb00716e8e0a0e07b87a39dc1a77fa
Pedro Neto <stabilepedro010403@gmail.com>
2024-10-03 13:45:42 -0300

    consertando os niveis de usuarios nas rotas

--- src/layouts/full/header/Header.js
+++ src/layouts/full/header/Header.js
@@ -63,7 +63,6 @@ const Header = (props) => {
         </IconButton>
         <Box flexGrow={1} />
         <Stack spacing={1} direction="row" alignItems="center">
-          
           <Profile />
         </Stack>
       </ToolbarStyled>
