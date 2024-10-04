1d12c4d5d5af71490e21cc0be49572239a6b63f3
Pedro Neto <stabilepedro010403@gmail.com>
2024-10-03 21:22:22 -0300

    adicionando botões de novo recurso e relatórios

--- src/components/recursos/list.js
+++ src/components/recursos/list.js
@@ -43,11 +43,20 @@ const RecursosList = () => {
     setMenuAccess(updatedMenu);
   };
 
+  const handleNewUser = () => {
+    navigate('/missoes/create');
+  };
+
   return (
     <div style={{ maxWidth: '100%', padding: '20px' }}>
       <Typography variant="h4" gutterBottom>
         Recursos da Primeira Igreja Batista de Presidente Prudente
       </Typography>
+
+      <div className="d-flex justify-content-between mb-3" style={{marginTop: '2%'}}>
+        <button className="btn btn-success" onClick={handleNewUser}><IconClipboard /> Gerar Relatório</button>
+        <button className="btn btn-primary" onClick={handleNewUser}><IconPlus /> Novo Recurso</button>
+      </div>
 
       <AppBar position="static" style={{marginTop: '2%'}} color="default">
         <Tabs value={activeTab} onChange={handleTabChange} indicatorColor="primary" textColor="primary" variant="fullWidth">
@@ -119,6 +128,176 @@ const RecursosList = () => {
       )}
 
        {activeTab === 1 && (
+        <Box mt={3}>
+            <Table sx={{ marginTop: '2%' }}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                        <Typography variant="subtitle2" fontWeight={600}>
+                            ID
+                        </Typography>
+                        </TableCell>
+                        <TableCell>
+                        <Typography variant="subtitle2" fontWeight={600}>
+                            Nome
+                        </Typography>
+                        </TableCell>
+                        <TableCell>
+                        <Typography variant="subtitle2" fontWeight={600}>
+                            Tipo
+                        </Typography>
+                        </TableCell>
+                        <TableCell>
+                        <Typography variant="subtitle2" fontWeight={600}>
+                            Quantidade
+                        </Typography>
+                        </TableCell>
+                        <TableCell align="right">
+                        <Typography variant="subtitle2" fontWeight={600}>
+                            Ações
+                        </Typography>
+                        </TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow key='1'>
+                            <TableCell>
+                            <Typography variant="body2">1</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">Revista Nova Vida em Cristo I</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">Revista</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">40</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                            <IconMinus />
+                            <IconPlus />
+                            </TableCell>
+                        </TableRow>
+                        <TableRow key='2'>
+                            <TableCell>
+                            <Typography variant="body2">2</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">Revista Nova Vida em Cristo II</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">Revista</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">20</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                            <IconMinus />
+                            <IconPlus />
+                            </TableCell>
+                        </TableRow>
+                        <TableRow key='3'>
+                            <TableCell>
+                            <Typography variant="body2">3</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">Comentário expositivo evangelho segundo Lucas</Typography>
+                            </TableCell> 
+                            <TableCell>
+                            <Typography variant="body2">Livro</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">1</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                            <IconMinus />
+                            <IconPlus />
+                            </TableCell>
+                        </TableRow>
+                </TableBody>
+            </Table>
+        </Box>
+      )}
+
+      {activeTab === 2 && (
+        <Box mt={3}>
+          <Table sx={{ marginTop: '2%' }}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                        <Typography variant="subtitle2" fontWeight={600}>
+                            ID
+                        </Typography>
+                        </TableCell>
+                        <TableCell>
+                        <Typography variant="subtitle2" fontWeight={600}>
+                            Nome
+                        </Typography>
+                        </TableCell>
+                        <TableCell>
+                        <Typography variant="subtitle2" fontWeight={600}>
+                            Quantidade
+                        </Typography>
+                        </TableCell>
+                        <TableCell align="right">
+                        <Typography variant="subtitle2" fontWeight={600}>
+                            Ações
+                        </Typography>
+                        </TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow key='1'>
+                            <TableCell>
+                            <Typography variant="body2">1</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">Copos plásticos 100 unidades</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">40</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                            <IconMinus />
+                            <IconPlus />
+                            </TableCell>
+                        </TableRow>
+                        <TableRow key='2'>
+                            <TableCell>
+                            <Typography variant="body2">2</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">Calhamaço folhas A4</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">100</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                            <IconMinus />
+                            <IconPlus />
+                            </TableCell>
+                        </TableRow>
+                        <TableRow key='3'>
+                            <TableCell>
+                            <Typography variant="body2">3</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">Canetas BIC</Typography>
+                            </TableCell>
+                            <TableCell>
+                            <Typography variant="body2">70</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                            <IconMinus />
+                            <IconPlus />
+                            </TableCell>
+                        </TableRow>
+                </TableBody>
+            </Table>
+        </Box>
+      )}
+
+      {activeTab === 3 && (
         <Box mt={3}>
           <Table sx={{ marginTop: '2%' }}>
                 <TableHead>
@@ -209,56 +388,6 @@ const RecursosList = () => {
             </Table>
         </Box>
       )}
-
-      {/* {activeTab === 3 && (
-        <Box mt={3}>
-          <Typography variant="h6">Notificações</Typography>
-          <Typography variant="body2" color="textSecondary" gutterBottom>
-            Ativar ou desativar notificações por e-mail.
-          </Typography>
-          <FormControl fullWidth>
-            <Select value={notifications} onChange={(e) => setNotifications(e.target.value)}>
-              <MenuItem value="enabled">Ativadas</MenuItem>
-              <MenuItem value="disabled">Desativadas</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-      )}
-
-      {activeTab === 4 && (
-        <Box mt={3}>
-          <Typography variant="h6">Configurações de Acesso ao Menu</Typography>
-          <Typography variant="body2" color="textSecondary" gutterBottom>
-            Defina quais níveis de usuário podem acessar cada item do menu.
-          </Typography>
-          {menuAccess.map((item) =>
-            item.navlabel ? (
-              <Typography key={item.subheader} variant="h4" gutterBottom>
-                <hr />
-                <b>{item.subheader}:</b>
-              </Typography>
-            ) : (
-              <Box key={item.id} mb={2}>
-                <Typography variant="body1"><b>{item.title}</b></Typography>
-                <FormGroup row>
-                  {[1, 2, 3, 4].map((level) => (
-                    <FormControlLabel
-                      key={level}
-                      control={
-                        <Checkbox
-                          checked={item.allowedLevels.includes(level)}
-                          onChange={() => handleMenuAccessChange(item.id, level)}
-                        />
-                      }
-                      label={`Nível ${level}`}
-                    />
-                  ))}
-                </FormGroup>
-              </Box>
-            )
-          )}
-        </Box>
-      )}} */}
 
       <hr />
 
